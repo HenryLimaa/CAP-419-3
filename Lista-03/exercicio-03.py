f448c6d80cbabfc6bcee20201d2a2d16e6da7f88
HenryLimaa <87545477+HenryLimaa@users.noreply.github.com>
2022-04-18 09:48:31 -0300

    Update exercicio-03.py

--- Lista-03/exercicio-03.py
+++ Lista-03/exercicio-03.py
@@ -3,9 +3,12 @@
 
 #Escreva um programa em Python que pergunte ao usuário um número entre 0 e 10, e diga se ele é ímpar ou par.
 
-numero = int(input('Digite um número entre 0 e 10: '))
+n = -1
 
-if (numero%2) == 0:
+while n < 0 or n > 10:
+  n = int(input('Digite um número entre 0 e 10: '))
+
+if (n%2) == 0:
   print("Par")
 else:
   print("Ímpar")

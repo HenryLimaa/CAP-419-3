68882a3d5df28cb3d746f803a407227092e415c8
HenryLimaa <87545477+HenryLimaa@users.noreply.github.com>
2022-04-17 22:42:04 -0300

    Update exercicio-01.py

--- Lista-04/exercicio-01.py
+++ Lista-04/exercicio-01.py
@@ -1,1 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+# Faça um programa para computar e apresentar a sequência de cada uma das seguintes séries numéricas, depois de ler um número inteiro N>=0 :
+
+import math
+
+Lucas = [2, 1, 3, 4, 7, 11, 18, 29, 47]
+print(Lucas)
+
+
+
+Pell = [0, 1, 2, 5, 12, 29, 70, 169, 408]
+print(Pell)
+
+
+
+Triangular = [0, 1, 3, 6, 10, 15, 21, 28, 36]
+print(Triangular)
+
+
+
+Square = [0, 1, 4, 9, 16, 25, 36, 49, 64]
+print(Square)
+
+
+
+Pentagonal = [0, 1, 5, 12, 22, 35, 51, 70, 92]
+print(Pentagonal)
